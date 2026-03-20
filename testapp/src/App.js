49771902e1f6d41ec4a1947cf15afe3e7adc63aa
Saurabh2341 <saurabh@futuristicbots.com>
2026-03-20 09:29:01 +0530

    Little change

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is demo project for docker Tutorial!!!!!.
+          This is demo project for docker Tutorial.
         </p>
         <a
           className="App-link"
