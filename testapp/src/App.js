8342f7a124704544e50de3c1895fd916844aa79b
Saurabh2341 <saurabh@futuristicbots.com>
2026-03-21 12:19:33 +0530

    changed for adani

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is testing for Adani.
+          Adani Testing.
         </p>
         <a
           className="App-link"
